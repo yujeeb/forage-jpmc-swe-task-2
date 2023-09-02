b64a5f9062c83dd74aaf50f14805c3a975ff4e01
Yujeeb Abbas Kashani <work.yujeeb@gmail.com>
2023-09-02 22:06:40 +0530

    set showGraph to false in the constructor

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,7 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    if (this.state.showGraph){
-      return (<Graph data={this.state.data}/>)
-    }
+    return (<Graph data={this.state.data}/>)
   }
 
   /**
