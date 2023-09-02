353a78ce4bff721ef6cc7dd39944d196f04fe399
Yujeeb Abbas Kashani <work.yujeeb@gmail.com>
2023-09-02 22:18:10 +0530

    Simplified the const elem definition

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
